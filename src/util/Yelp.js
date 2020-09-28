18bfbf51047178a6fb060c345e07940260ce8804
Joel Barbosa <joelbarbosa@gmail.com>
2020-09-28 11:00:57 +0100

    first commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey =
+  "W5-zCVdezv26ZwWLhEhkT4mWqvC50d2yHrCut9aJG7cUn92ZGwW78sa4bpvK-jOAzzM9MN99L9xaRQmgN73pwDODWljDtVqD3ZsgDgu6cBnOV7bNMUHVBkhmMIFeX3Yx";
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          }));
+        }
+      });
+  },
+};
+
+export default Yelp;
